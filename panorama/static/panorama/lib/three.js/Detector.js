891c163c3468621fb3e6cd1a0b5cd9b99cd36623
Tank <Ck014317>
2016-12-04 17:06:42 +0800

    判断浏览器对webgl是否支持

--- panorama/static/panorama/lib/three.js/Detector.js
+++ panorama/static/panorama/lib/three.js/Detector.js
@@ -4,46 +4,47 @@
  */
 
 var Detector = {
+
     canvas: !!window.CanvasRenderingContext2D,
     webgl: (function () {
         try {
             var canvas = document.createElement('canvas');
             return !!( window.WebGLRenderingContext && ( canvas.getContext('webgl') || canvas.getContext('experimental-webgl') ) );
-
         } catch (e) {
             return false;
         }
-
     })(),
     workers: !!window.Worker,
     fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+
     getWebGLErrorMessage: function () {
+
         var element = document.createElement('div');
         element.id = 'webgl-error-message';
         element.style.fontFamily = 'monospace';
         element.style.fontSize = '13px';
         element.style.fontWeight = 'normal';
         element.style.textAlign = 'center';
-        element.style.background = '#fff';
+        element.style.background = '#ffd598';
         element.style.color = '#000';
-        element.style.padding = '1.5em';
-        element.style.width = '400px';
-        element.style.margin = '5em auto 0';
-        if (!this.webgl) {
-            element.innerHTML = window.WebGLRenderingContext ? [
-                '您的浏览器似乎不支持 <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>。<br />',
-                '点击<a href="http://get.webgl.org/" style="color:#000">这里</a>查看如何获取WebGL支持'
-            ].join('\n') : [
-                '您的浏览器似乎不支持 <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>。<br />',
-                '点击<a href="http://get.webgl.org/" style="color:#000">这里</a>查看如何获取WebGL支持'
-            ].join('\n');
+        element.style.padding = '1em 0';
+        element.style.position = 'absolute';
+        element.style.top = '0';
+        element.style.left = '0';
+        element.style.width = '100%';
+        element.style.zIndex = '10';
 
+        if (!this.webgl) {
+            element.innerHTML = window.WebGLRenderingContext ?
+                '您的显卡似乎不支持 <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>。<br />点击 <a href="http://get.webgl.org/" style="color:#000">这里</a> 查看如何获取WebGL支持'
+                :
+                '您的浏览器似乎不支持 <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>。<br />点击 <a href="http://get.webgl.org/" style="color:#000">这里</a> 查看如何获取WebGL支持';
         }
         return element;
+
     },
 
     addGetWebGLMessage: function (parameters) {
-        document.body.className = '';
         var parent, id, element;
         parameters = parameters || {};
         parent = parameters.parent !== undefined ? parameters.parent : document.body;
@@ -52,11 +53,4 @@ var Detector = {
         element.id = id;
         parent.appendChild(element);
     }
-
 };
-
-// browserify support
-if (typeof module === 'object') {
-    module.exports = Detector;
-
-}

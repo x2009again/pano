92366210c76b2bc304c45225943b7e6ab97d9bdb
chenkai <just_be_fine@163.com>
2018-07-07 16:48:38 +0800

    去掉调试模式

--- root/settings.py
+++ root/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(m^d%#x%jqv@+^&qg8^-8r485%pf&*)7hx-01)&x#&2$6w_)ac'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition

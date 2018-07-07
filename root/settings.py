a0825c925677fc1b23fabeacbd3b125875d8f78b
chenkai <just_be_fine@163.com>
2018-07-07 16:53:29 +0800

    数据库配置修改

--- root/settings.py
+++ root/settings.py
@@ -77,18 +77,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'mysql': {
-        'ENGINE': 'django.db.backends.mysql',
-        'PORT': '3306',
-        'NAME': 'panorama',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-    },
-    'sqlite3': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'mysql': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'PORT': '3306',
+    #     'NAME': 'panorama',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '127.0.0.1',
+    # },
+    # 'sqlite3': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
